8b50d1d33b951f6eab1272f9897066e9e1a5e724
rkmonarch <rahulkulkarni5234@gmail.com>
2025-11-10 20:46:00 +0530

    fix: image

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+// app/head.tsx
+export default function Head() {
+    return (
+      <>
+        <title>Rahul Kulkarni</title>
+        <meta name="description" content="Builds on Solana • DevRel • Infrastructure • Movies & Trails" />
+        <link rel="icon" href="https://pbs.twimg.com/profile_images/1887132596532027392/GjV-2EHa_400x400.jpg" />
+      </>
+    );
+  }
+  
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata = {
   title: 'Rahul Kulkarni',
   description: 'Builds on Solana • DevRel • Infrastructure • Movies & Trails',
   icons: {
-    icon: '/dark.jpg', // make sure rk.png is in the public folder
+    icon: '/rk.jpg',
   },
 };
 
@@ -19,16 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-black text-white antialiased min-h-screen flex flex-col justify-between">
-        {/* Header */}
-        <header className="max-w-6xl mx-auto px-8 py-4 w-full">
+        <header className="max-w-6xl mx-auto px-2 md:px-4 lg:px-6 py-4 w-full">
           <Nav />
         </header>
-
-        {/* Main Content */}
-        <main className="flex-grow max-w-6xl px-8 mx-auto">{children}</main>
-
-        {/* Footer */}
-        <footer className="max-w-6xl mx-auto px-8 py-10 text-center text-gray-400 border-t border-[#1A1F27] mt-auto text-sm">
+        <main className="flex-grow max-w-6xl px-2 md:px-4 lg:px-6 mx-auto">{children}</main>
+        <footer className="max-w-6xl mx-auto px-2 md:px-4 lg:px-6 py-10 text-center text-gray-400 border-t border-[#1A1F27] mt-auto text-sm">
           <div>Built with ☕ and a love for reliable infra.</div>
           <div className="mt-2">Rahul Kulkarni • {new Date().getFullYear()}</div>
         </footer>
